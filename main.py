9ed19938a576f131ce6b47da1d9e374a9ed9574c
coolunderstanding37 <rael.brown96@gmail.com>
2024-11-07 00:31:27 -0500

    removed 'if' function

--- main.py
+++ main.py
@@ -4,8 +4,6 @@
 import pygame
 from constants import *
 
-print(f"Starting asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
-if __name__ == "__main__":
-	main()
+	print(f"Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
