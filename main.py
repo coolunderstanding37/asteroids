1249750eb84ca030dd7053c3957e3a439a3b6dad
coolunderstanding37 <rael.brown96@gmail.com>
2024-11-08 00:39:01 -0500

    implemented exit function

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from constants import *
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-while True:
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
-
 print(f"Starting asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
+
+while True:
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+
