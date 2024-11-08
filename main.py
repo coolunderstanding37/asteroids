02086b3a9fc7aae31573dabd35098ab219ee3936
coolunderstanding37 <rael.brown96@gmail.com>
2024-11-08 01:10:48 -0500

    implemented a pause function for when window is inactive

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -10,11 +11,22 @@ print(f"Starting asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
+paused = False
+
 while True:
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
+        # Handle active event to toggle paused state
+        if event.type == pygame.ACTIVEEVENT:
+            if event.gain == 0:    # window not active - pause game
+                paused = True
+            if event.gain == 1:    # window is active - game not paused
+                paused = False
+
+    if not paused:
+        # run game update and rendering logic only if not paused
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
